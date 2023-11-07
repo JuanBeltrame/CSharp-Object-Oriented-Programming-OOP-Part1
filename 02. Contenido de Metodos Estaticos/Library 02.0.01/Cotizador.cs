b870c8e0fe6e24bd2d364d93d6e63b4ff01bd89c
JuanBeltrame <jbeltrame@frro.utn.edu.ar>
2023-11-07 15:49:32 -0300

    Rename Files

---
+++ 02. Contenido de Metodos Estaticos/Library 02.0.01/Cotizador.cs
@@ -0,0 +1,22 @@
+﻿namespace Library_02._0._01
+{
+    public static class Cotizador
+    {
+        private const decimal pesosPorDolarComprado = 96.75M;
+        private const decimal pesosPorDolarVendido = 102.82M;
+
+        public static decimal CotizarVentaDolares(decimal montoDolaresAVender)
+        {
+            decimal costoEnPesos = montoDolaresAVender * pesosPorDolarVendido;
+
+            return costoEnPesos;
+        }
+
+        public static decimal CotizadorCompraDolares(decimal montoDolaresAComprar)
+        {
+            decimal pagoEnPesos = montoDolaresAComprar * pesosPorDolarComprado;
+
+            return pagoEnPesos;
+        }
+    }
+}
---
+++ 02. Contenido de Metodos Estaticos/View For Static Methods/Program.cs
@@ -0,0 +1,10 @@
+﻿using Library_02._0._01;
+
+decimal valorObetidoEnDolares = Cotizador.CotizadorCompraDolares(100);
+
+decimal valorObtenidoEnPesos = Cotizador.CotizarVentaDolares(100);
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Datos Cotizaciones: ");
+Console.WriteLine(valorObetidoEnDolares);
+Console.WriteLine(valorObtenidoEnPesos);
