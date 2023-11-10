8e709b610eefc4b1a102dd5b20edf6a2e7a4ae5c
Juan Beltrame <juan_beltrame@hotmail.com>
2023-11-10 20:40:37 -0300

    Update

--- 00. Trabajando con Clases/Console to work with Strings/Program.cs
+++ 00. Trabajando con Clases/Console to work with Strings/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Variables 
+string nombreCompleto; // Asi se declara
+nombreCompleto = "Juan Beltrame"; // Asi se asigna  
+string n = "Hello Word " + nombreCompleto + "!";  // Declarar y asignar en la misma linea 
+Console.WriteLine(nombreCompleto); // mostrar el mensaje por consola 
