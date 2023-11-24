ebf6a98eec4ee46d6bd20d329bcd550b13c4afb2
JuanBeltrame <jbeltrame@frro.utn.edu.ar>
2023-11-24 11:18:40 -0300

    Update Files

---
+++ ObjectOrientedProgrammingOOP/03. Contenido de Objetos/Console Example 03.0.03/Program.cs
@@ -0,0 +1,25 @@
+﻿using Console_Example_03._0._03;
+
+
+Galletita g1 = new Galletita("Vainilla", 300);
+Galletita g2 = new Galletita("Chocolate", 150);
+
+
+Galletita[] paqueteDeGalletitas = new Galletita[8];
+
+for (int i = 0; i < paqueteDeGalletitas.Length; i++)
+{
+    paqueteDeGalletitas[i] = new Galletita("Vainilla", 300);
+}
+if (paqueteDeGalletitas[0] == paqueteDeGalletitas[1])
+{
+
+}
+if (g1 == g2)
+{
+
+}
+if (paqueteDeGalletitas[0].GetSabor() == paqueteDeGalletitas[1].GetSabor())
+{
+
+}
