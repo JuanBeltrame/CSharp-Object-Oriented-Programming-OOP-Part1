2a7c947ff5c55655971e63543e610cd554ac5549
Juan Beltrame <juan_beltrame@hotmail.com>
2023-12-08 15:07:06 -0300

    Updates

---
+++ 00. Trabajando con Clases/Console to work with Random/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq.Expressions;
+
+int valor01;
+int valor02;
+int valor03;
+int tamanio = 4;
+byte[] valor04 = new byte[tamanio];
+double valor05 = 0;
+float valor06 = 0;
+
+// Opcion, trabajar dentro de un metodo
+int GenerarNumero()
+{
+    Random random = new Random();
+
+    return random.Next();
+}
+
+int GetAnotherRandomNumber()
+{
+    Random random = new Random();
+
+    return random.Next(1000);
+}
+
+
+void ByteRandomNumber(byte[] valorCuatro)
+{
+    Random random = new Random();
+    random.NextBytes(valorCuatro);
+    for (int i = 0; i < tamanio; i++)
+    {
+        Console.WriteLine(valorCuatro[i]);
+    }
+}
+
+void GetRandomDouble(ref double valorCinco)
+{
+    Random random1 = new Random();
+    valorCinco = random1.NextDouble();
+}
+
+void GetRandomFloat(ref float valorSeis)
+{
+    Random random1 = new Random();
+    valorSeis = random1.NextSingle();
+}
+
+
+//------------Programa Principal------------
+
+// Trabajar con Random dentro del programa principal
+Random random = new Random();
+valor02 = random.Next(1, 100);
+Console.WriteLine(valor02);
+
+valor01 = GenerarNumero();
+Console.WriteLine(valor01);
+
+valor03 = GetAnotherRandomNumber();
+Console.WriteLine(valor03);
+
+ByteRandomNumber(valor04);
+
+GetRandomDouble(ref valor05);
+Console.WriteLine(valor05);
+
+GetRandomFloat(ref valor06);
+Console.WriteLine(valor06);
--- 00. Trabajando con Clases/Console to work with TimeSpan/Program.cs
+++ 00. Trabajando con Clases/Console to work with TimeSpan/Program.cs
@@ -1,4 +1,34 @@
-﻿// Crear un nuevo objeto TimeSpan
+﻿TimeSpan time01;
+
+TimeSpan Metodo1conTimeSpan()
+{
+    TimeSpan timeSpan = new TimeSpan(-1, -30, -45); // Un TimeSpan negativo
+
+    // Obtener la duración absoluta
+    TimeSpan time = timeSpan.Duration();
+
+    return time;
+}
+
+void Metodo2conTimeSpan()
+{
+    TimeSpan miTiempoSpan = new TimeSpan(1, 2, 30, 45); // 1 día, 2 horas, 30 minutos, 45 segundos
+  
+}
+
+// ---------Programa Principal-----------
+
+time01 = Metodo1conTimeSpan();
+Console.WriteLine(time01);
+
+
+
+
+
+
+
+
+// Crear un nuevo objeto TimeSpan
 TimeSpan interval = new TimeSpan(2, 14, 18);  // 2 horas, 14 minutos, 18 segundos
 Console.WriteLine(interval.ToString()); // Imprime "02:14:18"
 
@@ -11,3 +41,4 @@ Console.WriteLine(difference.Days); // Imprime el número de días entre start y
 // Agregar a un TimeSpan
 TimeSpan addedInterval = interval.Add(new TimeSpan(1, 0, 0)); // Agrega una hora
 Console.WriteLine(addedInterval.ToString()); // Imprime "03:14:18"
+
