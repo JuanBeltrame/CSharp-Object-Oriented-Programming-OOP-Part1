ceafd74ad35a9307f6c6aa32c6a8e8b8fbeaddf7
Juan Beltrame <juan_beltrame@hotmail.com>
2023-12-22 23:11:47 -0300

    Update

---
+++ Console to work with Math/Program.cs
@@ -0,0 +1,25 @@
+﻿//------------Programa Principal------------------
+/* 
+----------------------------------------------------------------------
+English:
+Enter a number and display the square and the cube of the number. 
+You must validate that the number is greater than zero, 
+otherwise, display the message: "ERROR. Re-enter number!".
+
+Spanish: 
+Ingresar un número y mostrar el cuadrado y el cubo del mismo. 
+Se debe validar que el número sea mayor que cero, 
+caso contrario, mostrar el mensaje: "ERROR. ¡Reingresar número!".
+----------------------------------------------------------------------
+*/
+
+//------------Programa Principal-------------
+Console.Write("Ingresar un numero: ");
+while(!int.TryParse(Console.ReadLine(), out int number))
+{
+    Console.WriteLine("Error, se ingreso un caracter que no es un numero. Intente nuevamente");
+    Console.Write("Ingresar un numero: ");
+}
+
+
+Console.ReadKey();
