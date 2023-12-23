17c7c20a34476c716c51805972a5cf2fb3f2d982
Juan Beltrame <juan_beltrame@hotmail.com>
2023-12-23 20:36:53 -0300

    Update

---
+++ 04. Contenido de Sobrecarga/Method Overloading Examples/Calculadora.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloading_Examples
+{
+    internal class Calculadora
+    {
+        // Sobrecarga con distinta cantidad de parametros
+        public static int Sumar(int operando01, int operando02, int operando03, int operando04)
+        {
+            return operando01 + operando02 + operando03 + operando04;
+        }
+
+        public static int Sumar(int operando01, int operando02)
+        {
+            return Sumar(operando01, operando02, 0, 0);
+        }
+
+        // Sobrecarga con distinto tipo de parametros
+        public static float Sumar(float operando01, float operando02, float operando03, float operando04)
+        {
+            return operando01 + operando02 + operando03 + operando04;
+        }
+
+        // Sobrecarga con distinto orden de parametros
+        public static int Sumar(int operando01, string operando02)
+        {
+            return Sumar(operando01, int.Parse(operando02));
+        }
+
+        public static void Sumar(ref string mensaje, int operando01)
+        {
+            mensaje = operando01.ToString() + mensaje;
+        }
+
+    }
+}
--- 04. Contenido de Sobrecarga/Method Overloading Examples/Program.cs
+++ 04. Contenido de Sobrecarga/Method Overloading Examples/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Method_Overloading_Examples;
+
+int resultado01 = Calculadora.Sumar(1, 2, 3, 4);
+Console.WriteLine(resultado01);
+
+int resultado02 = Calculadora.Sumar(1, 2);
+Console.WriteLine(resultado02);
+
+float resultado03 = Calculadora.Sumar(1.2f, 1.2f, 1.2f, 1.2f);
+Console.WriteLine(resultado03);
+
+int resultado04 = Calculadora.Sumar(1, "2");
+Console.WriteLine(resultado04);
+
+string? mensaje =default;
+Calculadora.Sumar(ref mensaje!, 2);
+Console.WriteLine(mensaje);
