326e2d88c21bb6dbd6ac52b3edf52da7c9a57f25
Juan Beltrame <juan_beltrame@hotmail.com>
2023-12-23 08:41:31 -0300

    Update Program.cs

--- Console to work with Math/Program.cs
+++ Console to work with Math/Program.cs
@@ -1,5 +1,4 @@
-﻿//------------Programa Principal------------------
-/* 
+﻿/* 
 ----------------------------------------------------------------------
 English:
 Enter a number and display the square and the cube of the number. 
@@ -14,12 +13,49 @@ caso contrario, mostrar el mensaje: "ERROR. ¡Reingresar número!".
 */
 
 //------------Programa Principal-------------
-Console.Write("Ingresar un numero: ");
-while(!int.TryParse(Console.ReadLine(), out int number))
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+double number;
+char desicion;
+char desicionInicial = 'S';
+while (desicionInicial != 'N')
 {
-    Console.WriteLine("Error, se ingreso un caracter que no es un numero. Intente nuevamente");
+
     Console.Write("Ingresar un numero: ");
+    while (!double.TryParse(Console.ReadLine(), out number))
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Error, se ingreso un caracter que no es un numero. Intente nuevamente");
+        Console.Write("Ingresar un numero: ");
+
+    }
+
+    while (number <= 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine();
+        Console.WriteLine("Error, el numero debe ser mayor a cero. Intente nuevamente");
+        Console.Write("Ingresar un numero: ");
+        double.TryParse(Console.ReadLine(), out number);
+    }
+
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.WriteLine($"{number} elevado al cuadrado es: " + Math.Pow(number, 2));
+    Console.WriteLine($"{number} elevado al cubo es: " + Math.Pow(number, 3));
+
+    do
+    {
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.Write("Desea Ingresar otro numero? [S/N]: ");
+    } while (!char.TryParse(Console.ReadLine(), out desicion));
+    desicionInicial = desicion;
+
 }
 
-
+Console.WriteLine("Gracias por haber usado el servicio.");
 Console.ReadKey();
+
