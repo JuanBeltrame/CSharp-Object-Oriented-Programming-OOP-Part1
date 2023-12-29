94e8f1fec11b60b1a53a29200c5fe5607753d8e9
JuanBeltrame <jbeltrame@frro.utn.edu.ar>
2023-12-29 12:56:43 -0300

    UPdate

---
+++ 04. Contenido de Sobrecarga/Operator Overloading Examples/Aula.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overloading_Examples
+{
+    internal class Aula
+    {
+        private Persona[] personas;
+
+        public Aula()
+        {
+            personas = new Persona[4];
+        }
+
+        public static Aula operator +(Aula a, Persona p)
+        {
+            if (p != a)
+            {
+                for (int i = 0; i < a.personas.Length; i++)
+                {
+                    if (a.personas[i] is null)
+                    {
+                        a.personas[i] = p;
+                        return a;
+                    }
+                }
+            }
+            return a;
+        }
+
+        public static bool operator ==(Persona p, Aula a)
+        {
+            for (int i = 0; i < a.personas.Length; i++)
+            {
+                if (a.personas[i] == p)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool operator !=(Persona p, Aula a)
+        {
+            return !(p == a);
+        }
+    }
+}
--- 04. Contenido de Sobrecarga/Operator Overloading Examples/Program.cs
+++ 04. Contenido de Sobrecarga/Operator Overloading Examples/Program.cs
@@ -1,5 +1,5 @@
 ﻿using Operator_Overloading_Examples;
-
+//----Ejemplo 01-----
 Tiempo t1 = new Tiempo(21, 45, 59);
 Tiempo t2 = new Tiempo(21, 45, 59);
 Tiempo t3 = new Tiempo(12, 20, 17);
@@ -7,3 +7,19 @@ Tiempo t3 = new Tiempo(12, 20, 17);
 Console.WriteLine(t1 == t2);
 Console.WriteLine(t1 != t2);
 Console.WriteLine(t1 != t3);
+
+//----Ejemplo 02-----
+Persona persona1 = new Persona();
+Persona persona2 = new Persona("Juan", DateTime.Now, 33334545);
+Persona persona3 = new Persona("Juan", new DateTime(1998, 5, 9));
+Persona persona4 = new Persona("Ariel", new DateTime(1998, 5, 9), 32713684);
+
+Console.WriteLine(persona1 == persona2);
+
+Aula aula = new Aula();
+
+aula += persona1;
+aula += persona2;
+aula += persona3;
+aula += persona4;
+
