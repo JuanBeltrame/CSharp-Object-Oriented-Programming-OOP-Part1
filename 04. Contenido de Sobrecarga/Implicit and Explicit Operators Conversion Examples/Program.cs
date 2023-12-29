d4646a6ee3e7db06ca7e55ffe872c1951e75f3f2
JuanBeltrame <jbeltrame@frro.utn.edu.ar>
2023-12-29 16:38:52 -0300

    Update

---
+++ 04. Contenido de Sobrecarga/Implicit and Explicit Operators Conversion Examples/Program.cs
@@ -0,0 +1,8 @@
+﻿using Implicit_and_Explicit_Operators_Conversion_Examples;
+
+Tiempo t1 = new Tiempo(21, 45, 59);
+Tiempo t2 = new Tiempo(21, 45, 59);
+Tiempo t3 = new Tiempo(12, 20, 17);
+
+int segundos = t1;
+Console.WriteLine(segundos);
