804f33956c1989ee81be7652989741c477fb34c7
Juan Beltrame <juan_beltrame@hotmail.com>
2023-12-29 22:40:17 -0300

    Update

--- 04. Contenido de Sobrecarga/Implicit and Explicit Operators Conversion Examples/Program.cs
+++ 04. Contenido de Sobrecarga/Implicit and Explicit Operators Conversion Examples/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Implicit_and_Explicit_Operators_Conversion_Examples;
 
+//---Ejemplo 01----
 Tiempo t1 = new Tiempo(21, 45, 59);
 Tiempo t2 = new Tiempo(21, 45, 59);
 Tiempo t3 = new Tiempo(12, 20, 17);
@@ -9,3 +10,10 @@ Console.WriteLine(segundos);
 
 Tiempo tiempo = (Tiempo)"12:45:20";
 Console.WriteLine($"Hkora {tiempo.hora} minutos: {tiempo.minutos} segundos: {tiempo.segundos}");
+
+//---Ejemplo 02----
+Persona persona = new Persona("Lautaro", DateTime.Now, 12343432);
+
+long dni = persona;
+
+string nombre = (string)persona;
