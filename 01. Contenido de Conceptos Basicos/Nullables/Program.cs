7dd695346fd71d95e5ebaa71790b998a6bcd2047
JuanBeltrame <jbeltrame@frro.utn.edu.ar>
2024-01-02 11:35:29 -0300

    Update

--- 01. Contenido de Conceptos Basicos/Nullables/Program.cs
+++ 01. Contenido de Conceptos Basicos/Nullables/Program.cs
@@ -1,3 +1,22 @@
-﻿int price01 = 0; // <- Value is Zero. 
+﻿int price01 = 0; // <- Value is Zero.
+                 
+
+// Opcion utilizando: ? // Viene a ser como la sintaxis corta
 int? price02 = null; // <- No value at all.
+
+if (price02 == null)
+{
+    Console.WriteLine(nameof(price02)+" Es null");
+}
+else
+{
+
+}
+
+// Opcion utilizando: Nullable
 Nullable<int> price03 = null; // <- No value at all.
+
+if (price03 == null)
+{
+    Console.WriteLine(nameof(price03) + " Es null");
+}
