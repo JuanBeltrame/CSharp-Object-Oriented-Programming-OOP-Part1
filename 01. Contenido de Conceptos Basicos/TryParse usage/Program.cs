67549e59d677055fac484956b256c8c532648591
JuanBeltrame <jbeltrame@frro.utn.edu.ar>
2024-01-03 15:11:49 -0300

    Update

---
+++ 01. Contenido de Conceptos Basicos/TryParse usage/Program.cs
@@ -0,0 +1,13 @@
+﻿string numString = "a123";
+int num;
+
+if(int.TryParse(numString, out num))
+{
+    Console.WriteLine("Converting Success: " + num);
+}
+else
+{
+    Console.WriteLine("Converting failed");
+}
+
+bool gotParsed = int.TryParse(numString, out num);
