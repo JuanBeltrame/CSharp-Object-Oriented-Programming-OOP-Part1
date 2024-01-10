a5171fab3e95ee17f08b666f2931bacce56c7619
Juan Beltrame <juan_beltrame@hotmail.com>
2024-01-09 22:03:59 -0300

    Update

--- 02. Contenido de Metodos Estaticos/02.1.03 View/Program.cs
+++ 02. Contenido de Metodos Estaticos/02.1.03 View/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+Ejercicio I03 - Conversor binario - UTN Avellaneda
+----------------------------------------------------------------------
+English:
+Create a class called Converter that has two (static) class methods:
+
+The method ConvertDecimalToBinary that converts an integer from the decimal system to the binary system.
+public string ConvertDecimalToBinary(int integerNumber) {}
+
+The ConvertBinaryADecimal method that converts an integer from the binary system to the decimal system.
+public int ConvertBinaryToDecimal(int integerNumber) {}
+
+Do NOT use the language shortcuts, do it using iteration and selection structures.
+
+
+Spanish: 
+Desarollar una clase llamada Conversor que posea dos métodos de clase (estáticos):
+
+El método ConvertirDecimalABinario que convierte un número entero del sistema decimal al sistema binario.
+public string ConvertirDecimalABinario(int numeroEntero) {}
+
+El método ConvertirBinarioADecimal que convierte un número entero del sistema binario al sistema decimal.
+public int ConvertirBinarioADecimal(int numeroEntero) {}
+
+NO utilizar los atajos del lenguaje, hacerlo mediante estructuras de iteración y selección.
+----------------------------------------------------------------------
+*/
+
+//--Programa Principal---
