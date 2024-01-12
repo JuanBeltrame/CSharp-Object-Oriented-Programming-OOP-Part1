589fd2ff0a9a56571c45879ee41c7ee2b381193b
Juan Beltrame <juan_beltrame@hotmail.com>
2024-01-11 22:40:40 -0300

    Update Converter.cs

--- 02. Contenido de Metodos Estaticos/02.1.03 Library/Converter.cs
+++ 02. Contenido de Metodos Estaticos/02.1.03 Library/Converter.cs
@@ -1,7 +1,15 @@
-﻿namespace _02._1._03_Library
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _02._1._03_Library
 {
     public class Converter
     {
+        
+        public static string DecimalToBinary(int numero)
+        {
+            numero /= 2;
+        }
 
+        // Generamos el metodo de 
     }
 }
