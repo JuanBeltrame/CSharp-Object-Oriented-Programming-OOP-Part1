2959b6998b740e0d7c39d512f7e6fc6b7594c791
JuanBeltrame <jbeltrame@frro.utn.edu.ar>
2024-01-12 11:16:34 -0300

    Update Converter.cs

--- 02. Contenido de Metodos Estaticos/02.1.03 Library/Converter.cs
+++ 02. Contenido de Metodos Estaticos/02.1.03 Library/Converter.cs
@@ -4,12 +4,26 @@ namespace _02._1._03_Library
 {
     public class Converter
     {
-        
+        // Generamos el metodo de clase que convierte de decimal a binario
         public static string DecimalToBinary(int numero)
         {
-            numero /= 2;
-        }
+            string valorBinario = string.Empty; // Variable que vamos a retornar
+            // Generamos dos variables, una para verificar si el resultado de la division es igual a 0 y otra para verificar el resto
+            int resultDiv = numero;
+            int restoDiv;
+            
+            if(resultDiv >= 0)
+            {
+                do
+                {
+                    restoDiv = resultDiv % 2;
+                    resultDiv /= 2;
+                    valorBinario = restoDiv.ToString() + valorBinario; // Lo hago de esta manera para ir dejando la ultima division en la primer posicion
+                } while (resultDiv > 0);
+            }
 
+            return valorBinario;
+        }
         // Generamos el metodo de 
     }
 }
