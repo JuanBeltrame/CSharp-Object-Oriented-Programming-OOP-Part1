22259879f4e7835a0b36e5ac3f2d7dd8d7b1d8d8
JuanBeltrame <jbeltrame@frro.utn.edu.ar>
2024-01-12 13:50:52 -0300

    Update

---
+++ 02. Contenido de Metodos Estaticos/02.1.05 Library/Calculadora.cs
@@ -0,0 +1,35 @@
+﻿namespace _02._1._05_Library
+{
+    public class Calculadora
+    {
+        public static decimal Calcular(decimal operando1, decimal operando2, string tipoOperacion)
+        {
+            decimal resultado = default;
+            switch (tipoOperacion)
+            {
+                case ("Suma"):
+                    resultado = operando1 + operando2;
+                    break;
+                case ("Resta"):
+                    resultado = operando1 - operando2;
+                    break;
+                case ("Multiplicacion"):
+                    resultado = operando1 * operando2;
+                    break;
+                case ("Division"):
+                    if (operando2 != 0)
+                    {
+                        resultado = operando1 / operando2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error, tiene que ser != 0");
+                        return -1;
+                    }
+                    break;
+            }
+
+            return resultado;
+        }
+    }
+}
