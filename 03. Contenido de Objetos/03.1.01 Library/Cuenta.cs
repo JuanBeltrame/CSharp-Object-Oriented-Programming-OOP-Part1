790ed350efa93403d3bfe82a17ef27994c0d4ddd
JuanBeltrame <jbeltrame@frro.utn.edu.ar>
2024-01-17 15:41:40 -0300

    Update

---
+++ 03. Contenido de Objetos/03.1.01 Library/Cuenta.cs
@@ -0,0 +1,59 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace _03._1._01_Library
+{
+    public class Cuenta
+    {
+        string titular;
+        decimal cantidad;
+
+        public Cuenta()
+        {
+            
+        }
+
+        public Cuenta(string titular, decimal cantidad)
+        {
+            this.titular = titular;
+            this.cantidad = cantidad;
+        }
+
+        public string GetTitular()
+        {
+            return titular;
+        }
+
+        public decimal GetCantidad()
+        {
+            return cantidad;
+        }
+
+        public StringBuilder Mostrar()
+        {
+            StringBuilder sb = new();
+
+            sb.AppendLine($"El titual de la cuenta es: {titular}");
+            sb.AppendLine($"El saldo de la cuenta es: {cantidad}");
+
+            return sb;
+        }
+
+        public void Ingresar(decimal cantidad)
+        {
+            if (cantidad > 0)
+            {
+                this.cantidad = cantidad;
+            }
+            else
+            {
+
+            }
+        }
+
+        public void Retirar(decimal monto)
+        {
+            cantidad = -monto;
+        }
+    }
+}
---
+++ 03. Contenido de Objetos/03.1.01 View/Program.cs
@@ -0,0 +1,12 @@
+﻿using _03._1._01_Library;
+
+Console.Title = "Creo que necesito un Prestamo";
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+
+Cuenta cuentaDeJuan = new Cuenta("Juan Beltrame", 0);
+
+Console.Write("Ingresar dinero a la cuenta: ");
+decimal ingresarDinero = decimal.Parse(Console.ReadLine()!);
+cuentaDeJuan.Ingresar(ingresarDinero);
+Console.WriteLine(cuentaDeJuan.Mostrar());
