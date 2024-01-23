0c107fd7b2a3ccaaceed5edb2b90fec966c30feb
JuanBeltrame <jbeltrame@frro.utn.edu.ar>
2024-01-23 16:53:30 -0300

    Update

--- 03. Contenido de Objetos/03.1.02 Library/Persona.cs
+++ 03. Contenido de Objetos/03.1.02 Library/Persona.cs
@@ -1,7 +1,56 @@
-﻿namespace _03._1._02_Library
+﻿using System;
+
+namespace _03._1._02_Library
 {
     public class Persona
     {
+        string nombre;
+        DateTime fechaDeNacimiento;
+        int dni;
 
+        public Persona(string nombre, DateTime fechaDeNacimiento, int dni)
+        {
+            this.nombre = nombre;
+            this.fechaDeNacimiento = fechaDeNacimiento;
+            this.dni = dni;
+        }
+
+        public string GetNombre()
+        {
+            return nombre;
+        }
+
+        public void SetNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public DateTime GetBirthDay()
+        {
+            return fechaDeNacimiento;
+        }
+
+        public void SetBirthDay(DateTime fechaDeNacimiento)
+        {
+            this.fechaDeNacimiento = fechaDeNacimiento;
+        }
+
+        public int GetDni()
+        {
+            return dni;
+        }
+
+        public void SetDNI(int dni)
+        {
+            this.dni = dni;
+        }
+
+
+        private TimeSpan CalcularEdad(DateTime fechaDeNacimiento)
+        {
+            DateTime diaActual = DateTime.Today;
+            TimeSpan edad = diaActual - fechaDeNacimiento;
+            return e;
+        }
     }
 }
--- 03. Contenido de Objetos/03.1.02 View/Program.cs
+++ 03. Contenido de Objetos/03.1.02 View/Program.cs
@@ -5,7 +5,25 @@ English:
 
 
 Spanish: 
+Crear una aplicación de consola y una biblioteca de clases que contenga la clase "Persona".
 
+Deberá tener los atributos:
+nombre,
+fechaDeNacimiento,
+dni,
+
+Deberá tener un "constructor" que inicialice todos los atributos.
+
+Construir los siguientes métodos para la clase:
+    
+. Setters y getters, para cada uno de los atributos.
+. CalcularEdad(), será privado y retornará la edad de la persona calculándola a partir de la fecha de nacimiento.
+. Mostrar(), retornará una cadena de texto con todos los datos de la persona, incluyendo la edad actual.
+. EsMayorDeEdad(), si es mayor de edad devuelve el valor “Es mayor de edad", sino devuelve “es menor”.
+
+Instanciar 3 objetos de tipo Persona en el método Main.
+
+Mostrar quiénes son mayores de edad y quiénes no.
 ----------------------------------------------------------------------
 */
 
