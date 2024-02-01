eabfc80d0e50042219d2e1bdd3e15b854d90fba5
JuanBeltrame <jbeltrame@frro.utn.edu.ar>
2024-02-01 16:57:22 -0300

    Update Producto.cs

--- 04. Contenido de Sobrecarga/04.1.04 Library/Producto.cs
+++ 04. Contenido de Sobrecarga/04.1.04 Library/Producto.cs
@@ -1,4 +1,6 @@
-﻿namespace _04._1._04_Library
+﻿using System.Text;
+
+namespace _04._1._04_Library
 {
     public class Producto
     {
@@ -11,6 +13,26 @@
             this.codigoDeBarra = codigoDeBarra;
             this.marca = marca;
             this.precio = precio;
+        }
+
+        public string GetMarca()
+        {
+            return marca;
+        }
+
+        public static string MostrarProducto(Producto p)
+        { 
+            return $"El codigo de barra del producto es: {p.codigoDeBarra}, la marca es: {p.marca}, y el precio es: {p.precio}" ;
+        }
+
+        public static bool operator ==(Producto a, Producto b)
+        {
+            return a.marca == b.marca && a.codigoDeBarra == b.codigoDeBarra;
+        }
+
+        public static bool operator !=(Producto a, Producto b)
+        {
+            return !(a == b);
         }
 
 
