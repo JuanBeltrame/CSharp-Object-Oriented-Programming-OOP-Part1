60966571f69aa1887f1b0b685807ca87f6600631
JuanBeltrame <jbeltrame@frro.utn.edu.ar>
2024-02-02 13:48:36 -0300

    Update

---
+++ 04. Contenido de Sobrecarga/04.1.04 Library/Estante.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04._1._04_Library
+{
+    public class Estante
+    {
+        Producto []productos;
+        int ubicacionEstante;
+
+        private Estante(int capacidad )
+        {
+            this.productos = new Producto[capacidad];
+        }
+
+        public Estante(int capacidad, int ubicacionEstante)
+            : this(capacidad)
+        {
+            this.ubicacionEstante = ubicacionEstante;
+        }
+
+        public Producto[] GetProductos()
+        {
+            return productos;
+        }
+
+        public static string MostrarEstante(Estante e)
+        {
+            StringBuilder sb = new();
+            sb.AppendLine($"Ubicacion: {e.ubicacionEstante}");
+            for (int i = 0; i < e.productos.Length; i++)
+            {
+                if (e.productos[i] is null)
+                {
+                    sb.AppendLine(Producto.MostrarProducto(e.productos[i]));
+                }
+            }
+            return $"Ubicacion del estante: {e.ubicacionEstante} y sus Productos: {e.GetProductos()}";
+        }
+
+        // Sobrecarga de Operadores
+        // ==: Retornará true si es que el producto ya se encuentra en el estante, false caso contrario.
+        public static bool operator ==(Estante e, Producto p)
+        {
+            for(int i = 0; i < e.productos.Length; i++)
+            {
+                if (e.productos[i] == p)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool operator !=(Estante e, Producto p)
+        {
+            return !(e == p);
+        }
+
+        // +: Retornará true y agregará el producto si el estante posee capacidad de almacenar al menos un producto más y dicho producto no se encuentra en el estante, false caso contrario.Reutilizar código.
+        public static bool operator +(Estante e, Producto p)
+        {
+            if(e != p)
+            {
+                for (int i = 0; i < e.productos.Length; i++)
+                {
+                    if (e.productos[i] is null)
+                    {
+                        e.productos[i] = p;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        // -: Retornará un estante sin el producto, siempre y cuando el producto se encuentre en el listado. Reutilizar código.
+        public static Estante operator -(Estante e, Producto p)
+        {
+            if(e == p)
+            {
+                for (int i = 0; i < e.productos.Length; i++)
+                {
+                    if (e.productos[i] == p)
+                    {
+                        e.productos[i] = null;
+                        break;
+                    }
+                }
+
+            }
+            return e;
+        }
+    }
+}
--- 04. Contenido de Sobrecarga/04.1.04 Library/Producto.cs
+++ 04. Contenido de Sobrecarga/04.1.04 Library/Producto.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Text.RegularExpressions;
 
 namespace _04._1._04_Library
 {
@@ -20,11 +21,18 @@ namespace _04._1._04_Library
             return marca;
         }
 
-        public static string MostrarProducto(Producto p)
-        { 
-            return $"El codigo de barra del producto es: {p.codigoDeBarra}, la marca es: {p.marca}, y el precio es: {p.precio}" ;
+        public float GetPrecio()
+        {
+            return precio;
         }
 
+        public static string MostrarProducto(Producto p)
+        {
+            return $"El codigo de barra del producto es: {p.codigoDeBarra}, la marca es: {p.marca}, y el precio es: {p.precio}";
+        }
+
+        // Sobrecarga de Operadores
+        // == (Producto, Producto): Retornará true si las marcas y códigos de barra son iguales, false caso contrario.
         public static bool operator ==(Producto a, Producto b)
         {
             return a.marca == b.marca && a.codigoDeBarra == b.codigoDeBarra;
@@ -34,6 +42,25 @@ namespace _04._1._04_Library
         {
             return !(a == b);
         }
+
+        // == (Producto, string): Retornará true si la marca del producto coincide con la cadena pasada como argumento, false caso contrario.
+        public static bool operator ==(Producto p, string marca)
+        {
+            return p.marca == marca;
+        }
+
+        public static bool operator !=(Producto p, string marca)
+        {
+            return !(p == marca);
+        }
+
+        // Sobrecarga de Operadores de Conversion
+        // explicit: Realizará la conversión de un objeto Producto a string. Sólo retornará el atributo codigoDeBarras del producto.
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarra;
+        }
+
 
 
     }
